6064194ff20eefab054a4e9a774a60cbc78ddd45
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-22 18:12:49 +0530

    Add files via upload

---
+++ PBFM4_Nested_if.py
@@ -0,0 +1,13 @@
+#Nested - If
+# Write a Python Program to Find big value among the 3 numbers
+
+#inputs
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+
+#condition
+
+
+
+
